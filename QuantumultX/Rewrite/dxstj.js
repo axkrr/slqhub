7aec0e8e631efaf14c87a3d08d29553613caebdd
x <135993682+axkrr@users.noreply.github.com>
2025-12-20 00:59:31 +0800

    更新 dxstj.js

--- QuantumultX/Rewrite/dxstj.js
+++ QuantumultX/Rewrite/dxstj.js
@@ -7,12 +7,17 @@ let body = $response.body;
 if (body) {
     try {
         let obj = JSON.parse(body);
+
+        // 修改倒计时渲染时间为 0
         if (obj.data) {
-            // 将倒计时渲染时间设为 0
             obj.data.renderTimeout = 0;
-            // 清空广告列表
+        }
+        
+        // 清空广告列表
+        if (obj.data) {
             obj.data.adList = [];
         }
+
         $done({ body: JSON.stringify(obj) });
     } catch (e) {
         $done({ body });
