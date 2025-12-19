b41ca140359c28a42317d92e48970804bcd7ed50
x <135993682+axkrr@users.noreply.github.com>
2025-12-20 01:04:02 +0800

    更新 dxstj.js

--- QuantumultX/Rewrite/dxstj.js
+++ QuantumultX/Rewrite/dxstj.js
@@ -7,22 +7,16 @@ let body = $response.body;
 if (body) {
     try {
         let obj = JSON.parse(body);
-
-        // 清空广告数据列表
-        if (obj.data && obj.data.adList) {
-            obj.data.adList = [];
+        if (obj.data) {
+            // 将渲染超时设为 0，实现秒跳
+            if (obj.data.adPosConfig) {
+                obj.data.adPosConfig.renderTimeout = 0;
+            }
+            // 清空广告列表
+            if (obj.data.codePosList) {
+                obj.data.codePosList = [];
+            }
         }
-        
-        // 修改渲染超时时间实现秒跳
-        if (obj.data && obj.data.renderTimeout) {
-            obj.data.renderTimeout = 0;
-        }
-
-        // 针对部分版本可能存在的额外配置
-        if (obj.data && obj.data.conf) {
-            obj.data.conf = {};
-        }
-
         $done({ body: JSON.stringify(obj) });
     } catch (e) {
         $done({ body });
