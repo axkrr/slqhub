904551e2c210ecaba29402478188919eedf4862f
x <135993682+axkrr@users.noreply.github.com>
2025-12-24 05:12:46 +0800

    更新 hlzh.js

--- Surge/Module/hlzh.js
+++ Surge/Module/hlzh.js
@@ -1,23 +1,7 @@
+
 /*
 * 航旅纵横去广告脚本
+* 简练功能说明：由于二进制强校验，本脚本已转为纯透传模式，去广告逻辑由 Rule 实现。
 */
 
-const url = $request.url;
-let body = $response.body;
-
-if (body) {
-    // 简练功能说明：针对初始化接口进行安全处理，若非 init 则原样返回避免报错
-    if (url.indexOf("/init") !== -1) {
-        let bodyString = body.toString();
-        // 简练功能说明：置零广告超时，开启黑名单
-        bodyString = bodyString.replace(/"advertTimeout":"\d+"/, '"0"')
-                               .replace(/"advertTotalTimeout":"\d+"/, '"0"')
-                               .replace(/"adBlackList":"0"/, '"1"');
-        $done({ body: bodyString });
-    } else {
-        // 简练功能说明：native等接口直接透传，不作修改以规避 GBP 校验错误
-        $done({ body });
-    }
-} else {
-    $done({});
-}
+$done({ body: $response.body });
