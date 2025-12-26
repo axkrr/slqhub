895b6731ee35eaa518eef5efe6cafa31b539b785
axkr <135993682+axkrr@users.noreply.github.com>
2025-12-26 21:31:52 +0800

    Update and rename QuantumultX/Rewrite/qd.js to QuantumultX/Rewrite/JS/qd.js

--- QuantumultX/Rewrite/JS/qd.js
+++ QuantumultX/Rewrite/JS/qd.js
@@ -1,6 +1,6 @@
 /**
  * @name qdpure_jsfile
- * @description quda ad blocking script
+ * @description 去哒 ad blocking script
  */
 
 let body = $response.body;
@@ -8,7 +8,6 @@ if (body) {
     try {
         let obj = JSON.parse(body);
         
-        // 清空所有广告数据列表
         if (obj.data) {
             obj.data = [];
         }
@@ -16,7 +15,6 @@ if (body) {
             obj.bid = [];
         }
         
-        // 保持状态码为成功
         obj.code = 1;
         obj.message = "成功";
         
