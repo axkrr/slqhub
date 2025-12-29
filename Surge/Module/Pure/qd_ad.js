6d5e7cacec9144743aa212a56a9ee8650a99d1b0
axkr <135993682+axkrr@users.noreply.github.com>
2025-12-29 10:37:20 +0800

    更新 qd_ad.js

--- Surge/Module/Pure/qd_ad.js
+++ Surge/Module/Pure/qd_ad.js
@@ -1,7 +1,15 @@
-// 伪造一个成功的空广告响应
-let obj = {
-  "code": 1,
-  "msg": "success",
-  "data": []
-};
-$done({body: JSON.stringify(obj), status: 200});
+/*
+去哒广告劫持脚本
+*/
+
+let obj = JSON.parse($response.body || "{}");
+
+// 强制清空所有可能存在的广告配置
+obj.data = []; 
+obj.code = 1; // 1 通常代表成功
+obj.msg = "success";
+
+$done({
+    body: JSON.stringify(obj),
+    status: 200 // 必须返回 200，让 App 觉得请求成功了
+});
