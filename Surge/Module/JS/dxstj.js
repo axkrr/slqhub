f7242dc15856b3505a2be727fc37118489668ab2
axkr <135993682+axkrr@users.noreply.github.com>
2025-12-31 10:37:22 +0800

    更新 dxstj.js

--- Surge/Module/JS/dxstj.js
+++ Surge/Module/JS/dxstj.js
@@ -1,26 +1,40 @@
 /**
  * @name dxstjpure_jsfile
- * @description daxuesoutijiang ad blocking script
+ * @desc 大学搜题酱去广告及秒跳修正版
  */
 
 let body = $response.body;
+
 if (body) {
     try {
         let obj = JSON.parse(body);
+        
         if (obj.data) {
-            // 将渲染超时设为 0，实现秒跳
+            // 1. 实现开屏/广告秒跳
             if (obj.data.adPosConfig) {
+                // 将超时和渲染时间设为 0
                 obj.data.adPosConfig.renderTimeout = 0;
+                obj.data.adPosConfig.requestTimeout = 0;
+                obj.data.adPosConfig.waitTimeout = 0;
             }
-            // 清空广告列表
+            
+            // 2. 清空广告列表
             if (obj.data.codePosList) {
                 obj.data.codePosList = [];
             }
+
+            // 3. 额外清理可能存在的其他广告字段 (参考之前的成功经验)
+            const extraKeys = ["adList", "bannerList", "configList"];
+            extraKeys.forEach(k => {
+                if (obj.data[k]) obj.data[k] = [];
+            });
         }
+
         $done({ body: JSON.stringify(obj) });
     } catch (e) {
+        // 解析失败则返回原体
         $done({ body });
     }
 } else {
-    $done({ body });
+    $done({});
 }
