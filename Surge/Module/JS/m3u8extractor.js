2413101c8066bec1b18786ec540a726227c7e948
axkr <135993682+axkrr@users.noreply.github.com>
2025-12-31 12:25:32 +0800

    更新 m3u8extractor.js

--- Surge/Module/JS/m3u8extractor.js
+++ Surge/Module/JS/m3u8extractor.js
@@ -1,77 +1,68 @@
 /**
- * @name 视频流嗅探 (复制+跳转版)
- * @desc 嗅探 m3u8，点击通知复制链接并唤起 SenPlayer
+ * @name 视频嗅探回退版 (复制+跳转)
+ * @desc 逻辑简单，只负责嗅探、复制链接、弹出通知并唤起播放器
  */
 
-const req = (typeof $request !== 'undefined') ? $request : null;
-const res = (typeof $response !== 'undefined') ? $response : null;
+const isRes = typeof $response !== "undefined";
 
-// --- 阶段 A: 处理信号，复制地址并弹出通知 ---
-if (req && req.url && req.url.indexOf('surge_click_to_play=') != -1) {
-    const videoUrl = decodeURIComponent(req.url.split('surge_click_to_play=')[1]);
+// --- A. 如果是信号请求：执行通知和复制 ---
+if ($request.url.indexOf('surge_click_to_play=') != -1) {
+    const videoUrl = decodeURIComponent($request.url.split('surge_click_to_play=')[1]);
     
-    // 1. 复制视频流地址到系统剪贴板
-    const copySuccess = $copy(videoUrl);
-    
-    // 2. 构造通知
-    // 唤起协议使用最基础的 senplayer:// 确保一定能打开 App
-    const senUrl = "senplayer://";
+    // 执行复制
+    $copy(videoUrl);
 
+    // 弹出通知 (只负责唤起，不负责自动播放，确保跳转成功)
     $notification.post(
-        "🎬 视频地址已复制",
-        "链接已入剪贴板，点击打开播放器",
-        "地址: " + videoUrl.split('?')[0].split('/').pop() + (copySuccess ? " (复制成功)" : ""),
-        { "open-url": senUrl }
+        "🎬 视频流捕获成功",
+        "链接已复制，点击打开播放器",
+        "源: " + videoUrl.split('?')[0].split('/').pop(),
+        { "open-url": "senplayer://" }
     );
-    
-    $done({
-        response: {
-            status: 204,
-            headers: { "Content-Type": "text/plain", "Access-Control-Allow-Origin": "*" },
-            body: ""
-        }
-    });
+
+    $done({ response: { status: 204, body: "" } });
 } 
 
-// --- 阶段 B: 注入内核 (保持不变) ---
-else if (res && res.body && res.body.indexOf('</head>') != -1) {
+// --- B. 如果是网页响应：注入嗅探代码 ---
+else if (isRes && $response.body && $response.body.indexOf('</head>') != -1) {
     const injectCode = `
     <script>
     (function() {
-        'use strict';
-        const foundUrls = new Set();
-        function sendToSurge(mUrl) {
-            if (mUrl && mUrl.indexOf('.m3u8') != -1 && !foundUrls.has(mUrl)) {
-                foundUrls.add(mUrl);
-                const img = new Image();
-                img.src = '/surge_click_to_play=' + encodeURIComponent(mUrl);
+        var found = new Set();
+        function send(url) {
+            if (url && url.indexOf('.m3u8') != -1 && !found.has(url)) {
+                found.add(url);
+                var i = new Image();
+                i.src = '/surge_click_to_play=' + encodeURIComponent(url);
             }
         }
-        const originalXHR = window.XMLHttpRequest.prototype.open;
-        window.XMLHttpRequest.prototype.open = function(method, url) {
-            if (typeof url === 'string' && url.indexOf('.m3u8') != -1) sendToSurge(url);
-            return originalXHR.apply(this, arguments);
+        // XHR 钩子
+        var open = XMLHttpRequest.prototype.open;
+        XMLHttpRequest.prototype.open = function() {
+            send(arguments[1]);
+            return open.apply(this, arguments);
         };
-        const originalFetch = window.fetch;
-        window.fetch = function(url, options) {
-            const tUrl = (typeof url === 'string') ? url : (url && url.url ? url.url : "");
-            if (tUrl && tUrl.indexOf('.m3u8') != -1) sendToSurge(tUrl);
-            return originalFetch.apply(this, arguments);
+        // Fetch 钩子
+        var oldFetch = window.fetch;
+        window.fetch = function(t) {
+            var u = (typeof t === 'string') ? t : (t.url || "");
+            send(u);
+            return oldFetch.apply(this, arguments);
         };
-        setInterval(() => {
-            const tags = document.querySelectorAll('video, source');
-            tags.forEach(t => {
-                const s = t.src || t.getAttribute('src');
-                if (s && s.includes('.m3u8')) sendToSurge(s);
-            });
-        }, 2000);
+        // 标签扫描
+        setInterval(function() {
+            var el = document.querySelectorAll('video, source');
+            for (var j=0; j<el.length; j++) {
+                send(el[j].src || el[j].getAttribute('src'));
+            }
+        }, 3000);
     })();
     </script>
     `;
-    $done({ body: res.body.replace('</head>', injectCode + '</head>') });
+    $done({ body: $response.body.replace('</head>', injectCode + '</head>') });
 } 
 
-// --- 阶段 C: 兜底退出 ---
+// --- C. 兜底 ---
 else {
     $done({});
 }
