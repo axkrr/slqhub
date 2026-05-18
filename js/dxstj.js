2a231f4ba6562bd0ff3bd6356b644c911e016d9c
axkr <135993682+axkrr@users.noreply.github.com>
2026-05-18 10:42:08 +0800

    Create dxstj.js

---
+++ js/dxstj.js
@@ -0,0 +1,51 @@
+/**
+ * @name dxstj
+ * @desc 大学搜题酱净化
+ * @author axkrr
+*/
+
+let body = $response.body;
+if (body) {
+    let obj = JSON.parse(body);
+    let url = $request.url;
+
+    // 依然保留以防App部分功能仍依赖旧接口
+    if (url.indexOf("init/config/init") !== -1) {
+        if (obj.data) {
+            if (obj.data.screen_ad) delete obj.data.screen_ad;
+            if (obj.data.splash) delete obj.data.splash;
+            obj.data.is_show_ad = false; 
+            obj.data.ad_config = [];
+        }
+    }
+
+    if (url.indexOf("init/config/popupconfig") !== -1) {
+        if (obj.data) {
+            obj.data.popupList = [];
+            if (obj.data.vipSales) obj.data.vipSales.needShow = false;
+        }
+    }
+
+    // 广告全局配置
+    if (url.indexOf("adxserver/ad/getconfig") !== -1) {
+        if (obj.data) {
+            obj.data.adnList = [];
+            obj.data.adList = [];
+            obj.data.splashShowAdIdMap = {};
+            obj.data.feedAdIdMap = {};
+            if (obj.data.config) {
+                obj.data.config.splashShowReplenishTime = 2147483647;
+                obj.data.config.pullIntervalTime = 2147483647;
+            }
+        }
+    }
+
+    // 广告具体载荷
+    if (url.indexOf("adxserver/ad/adreq") !== -1) {
+        obj.data = {};
+    }
+
+    $done({ body: JSON.stringify(obj) });
+} else {
+    $done({});
+}
