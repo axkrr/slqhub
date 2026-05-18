cf68657fe9721cfaced7244146d7a534db9594f2
axkr <135993682+axkrr@users.noreply.github.com>
2026-05-18 11:01:22 +0800

    Update 123pan.js

--- js/123pan.js
+++ js/123pan.js
@@ -2,6 +2,7 @@
  * @name 123panpure
  * @desc 123云盘净化
  * @author axkrr
+ * @update 2026-05-18
 */
 
 const url = $request.url;
@@ -10,19 +11,19 @@ let body = $response.body;
 try {
     let obj = JSON.parse(body);
 
-    // 处理Placement配置
+    // Placement配置
     if (url.indexOf('open/placement') !== -1) {
         obj.data = {};
         obj.msg = "Success";
         obj.code = 0;
     } 
-    // 处理Bid竞价
+    // Bid竞价
     else if (url.indexOf('/bid') !== -1) {
         obj.data = [];
         obj.msg = "success";
         obj.code = 0;
     } 
-    // 处理最新的Request广告内容请求
+    // Request广告内容请求
     else if (url.indexOf('/request') !== -1) {
         if (obj.data) {
             obj.data.seatbid = []; // 清空广告位列表
@@ -30,7 +31,7 @@ try {
         obj.msg = "success";
         obj.code = 0;
     }
-    // 处理领野广告Map
+    // 领野广告Map
     else if (url.indexOf('ad/map') !== -1) {
         obj.data = [];
         obj.bid = [];
