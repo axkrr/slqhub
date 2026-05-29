02f5b82ad15f379e591cf132a6d2a633b7b26187
axkr <135993682+axkrr@users.noreply.github.com>
2026-05-29 12:01:25 +0800

    更新 haijiao.js

--- js/haijiao.js
+++ js/haijiao.js
@@ -5,62 +5,20 @@
  * @update 2026-05-26
 */
 
-let { headers, url } = $request;
+let {headers, url} = $request;
+let isQuantumult = typeof $task !== 'undefined';
+let isSurge = typeof $httpClient !== 'undefined' && !isQuantumult;
+let isLoon = typeof $loon !== 'undefined';
 
-// 替换播放域名
-let playUrl = url
-  .replace(/\/\/(?!long)[^\.]+\./, "//y.js.cn.v7.")
-  .replace(/\.m3u8/, ".m3u8");
+let newUrl = url
+  .replace(/\/(?!long)[^\.]+\./, '//long.')
+  .replace('.m3u8', '.mp4');
 
-// 判断是否是真实视频流
-if (
-  headers["X-Playback-Session-Id"] ||
-  headers["x-playback-session-id"]
-) {
+headers.hasOwnProperty('X-Playback-Session-Id') && (
+  console.log('彭于晏提示❗️视频链接捕获成功'),
+  isQuantumult && $notify('彭于晏提示❗️视频链接捕获成功', '', '', {'open-url': newUrl}),
+  isSurge && $notify('彭于晏提示❗️视频链接捕获成功', '', '', {'url': newUrl}),
+  isLoon && $notification.post('彭于晏提示❗️视频链接捕获成功', '', '', {'openUrl': newUrl})
+);
 
-  console.log("视频链接捕获成功");
-  console.log(playUrl);
-
-  // qx
-  if (typeof $task !== "undefined") {
-    $notify(
-      "彭于晏提示❗️视频链接捕获成功",
-      "点击跳转播放",
-      "",
-      {
-        "open-url": playUrl
-      }
-    );
-  }
-
-  // surge
-  if (
-    typeof $httpClient !== "undefined" &&
-    typeof $task === "undefined"
-  ) {
-    $notification.post(
-      "彭于晏提示❗️视频链接捕获成功",
-      "点击跳转播放",
-      playUrl,
-      {
-        url: playUrl
-      }
-    );
-  }
-
-  // loon
-  if (typeof $loon !== "undefined") {
-    $notification.post(
-      "彭于晏提示❗️视频链接捕获成功",
-      "点击跳转播放",
-      playUrl,
-      {
-        openUrl: playUrl
-      }
-    );
-  }
-}
-
-$done({
-  headers
-});
+$done({response: {headers}});
