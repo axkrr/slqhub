b7c35b098725d5d4536264c03d409e0a63a47f33
axkr <135993682+axkrr@users.noreply.github.com>
2026-05-31 16:42:11 +0800

    创建 haijiao.js

---
+++ js/haijiao.js
@@ -0,0 +1,14 @@
+let { headers, url } = $request,
+  isSurge = typeof $task !== "undefined",
+  isQX = typeof $httpClient !== "undefined" && !isSurge,
+  isLoon = typeof $loon !== "undefined",
+  newUrl = url.replace(/\/\/(?!long)[^\.]+\./, "//long.").replace(/\.m3u8/, ".mp4");
+ 
+headers.hasOwnProperty("X-Playback-Session-Id") && (
+  console.log("通杀Crack~"),
+  isSurge && $notify("彭于晏提示❗️视频链接捕获成功", ">_ 点击此通知可跳转观看 🔞", "", { "open-url": newUrl }),
+  isQX && $notification.post("彭于晏提示❗️视频链接捕获成功", ">_ 点击此通知可跳转观看 🔞", "", { "url": newUrl }),
+  isLoon && $notification.post("彭于晏提示❗️视频链接捕获成功", ">_ 点击此通知可跳转观看 🔞", "", { "openUrl": newUrl })
+);
+ 
+$done({ response: { headers } });
